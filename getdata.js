82fb17e82a3cee5d5eb32e3ac362236c1adf395d
Kevzka <79326228+kevzka@users.noreply.github.com>
2024-09-08 00:38:46 +0800

    Delete getdata.js

--- getdata.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('http://192.168.43.138/whatsapp/get_data.php')
-    .then(response => {
-        console.log('Data yang diterima:', response.data);
-    })
-    .catch(error => {
-        console.error('Terjadi kesalahan:', error);
-    });
